be43a8f0ddc4a2570af40b68677dad2e2b4de759
Huy Vu Nguyen <huyk25sinh@gmail.com>
2024-01-19 06:01:01 +0000

    Update: learned user input cant be used with const variables, some basic syntaxes, data types conversion

--- C#/Program.cs
+++ C#/Program.cs
@@ -1,9 +1,25 @@
-﻿namespace C_;
+﻿using System; // like `import`
 
-class Program
+namespace HelloWorld // used to store classes and other namespaces
 {
-    static void Main(string[] args)
+  class Program
+  {
+    static void Main(string[] args) // a code that always will be 
     {
-        Console.WriteLine("Hello, World!");
+      int myNum = 91;
+      int divider = 3;
+      string myName = "Huy";
+      Console.WriteLine($"My Name is {myName}");
+      Console.WriteLine($"{myNum}%{divider} = {myNum%divider}");
+      Console.WriteLine($"Converted {myNum}/{divider} = {(float) myNum/divider}");
+      Console.WriteLine($"This is your input: [{string.Join(", ", args)}]");
+
+      Console.WriteLine("How old are you?");
+      // cant use `const`,
+      // because user input is a variable edited at runtime
+      int userAge = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine($"You are {userAge} years old");
+
     }
+  }
 }
